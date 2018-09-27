0a5004176b95e16a78c35cf9cb3dfcedff13a0eb
yyjun0801x <yyjun0801@foxmail.com>
2018-09-27 14:54:53 +0800

    创建联系对象
    Signed-off-by: yyjun0801x <yyjun0801@foxmail.com>

---
+++ Protocols/EPP/eppExtensions/verisign-1.10.0.3/eppResponses/verisignEppCreateContactResponse.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Metaregistrar\EPP;
+
+
+class verisignEppCreateContactResponse extends verisignBaseResponse
+{
+    /**
+     * CONTACT CREATE RESPONSES
+     */
+
+    /**
+     *
+     * @return string contact_id
+     */
+    public function getContactId() {
+        return $this->queryPath('/epp:epp/epp:response/epp:resData/contact:creData/contact:id');
+    }
+
+    /**
+     *
+     * @return string create_date
+     */
+    public function getContactCreateDate() {
+        return $this->queryPath('/epp:epp/epp:response/epp:resData/contact:creData/contact:crDate');
+    }
+
+    /**
+     *
+     * @return eppContactHandle contacthandle
+     */
+    public function getContactHandle() {
+        if ($handle = $this->queryPath('/epp:epp/epp:response/epp:resData/contact:creData/contact:id')) {
+            return new eppContactHandle($handle);
+        } else {
+            return null;
+        }
+    }
+
+}
