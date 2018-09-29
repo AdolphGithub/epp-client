27be36b080cb646f2c9af3306327f415122d8aaf
Adolph <1138027478@qq.com>
2018-09-29 11:05:36 +0800

    fix 修复appendExtension方法

--- Protocols/EPP/eppExtensions/verisign-1.10.0.3/eppRequests/verisignBaseRequest.php
+++ Protocols/EPP/eppExtensions/verisign-1.10.0.3/eppRequests/verisignBaseRequest.php
@@ -5,7 +5,7 @@ class verisignBaseRequest extends eppRequest
 {
     public function appendExtension($type = 'dotCOM')
     {
-        $namestoreExt = $this->setAttributes($this->createElement('namestoreExt:namestoreExt'),[
+        $namestoreExt = $this->setAttributes('namestoreExt:namestoreExt',[
             'xmlns:namestoreExt'    =>  'http://www.verisign-grs.com/epp/namestoreExt-1.1',
             'xmlns:xsi'             =>  'http://www.w3.org/2001/XMLSchema-instance',
             'xsi:schemaLocation'    =>  'http://www.verisign-grs.com/epp/namestoreExt-1.1 namestoreExt-1.1.xsd'
