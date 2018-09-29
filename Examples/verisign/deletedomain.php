2bc56f5432b8043129d917c192e6ec435051ffa7
Adolph <1138027478@qq.com>
2018-09-29 11:16:44 +0800

    add 添加完整例子

--- Examples/verisign/deletedomain.php
+++ Examples/verisign/deletedomain.php
@@ -11,6 +11,9 @@ use Guanjia\EPP\eppDomain;
 use Guanjia\EPP\eppContactHandle;
 use Guanjia\EPP\eppHost;
 use Guanjia\EPP\verisignEppCreateDomainRequest;
+use Guanjia\EPP\eppContactPostalInfo;
+use Guanjia\EPP\eppContact;
+use Guanjia\EPP\verisignEppCreateContactRequest;
 
 try {
     // Please enter your own settings file here under before using this example
@@ -68,10 +71,37 @@ function createdomain($conn, $domainname, $registrant, $admincontact, $techconta
 
 
     $create = new verisignEppCreateDomainRequest($domain);
+    /* @var $response Guanjia\EPP\eppCreateDomainResponse */
     if ($response = $conn->request($create)) {
         echo "Domain " . $response->getDomainName() . " created on " . $response->getDomainCreateDate() . ", expiration date is " . $response->getDomainExpirationDate() . "\n";
-        /* @var $response Guanjia\EPP\eppCreateDomainResponse */
         return $response->getDomainName();
     }
     return $domainname;
+}
+
+/**
+ * @param $conn eppConnection
+ * @param $email string
+ * @param $telephone string
+ * @param $name string
+ * @param $organization string
+ * @param $address string
+ * @param $postcode string
+ * @param $city string
+ * @param $country string
+ * @return null
+ */
+function createcontact($conn, $email, $telephone, $name, $organization, $address, $postcode, $city, $country) {
+    $postalinfo = new eppContactPostalInfo($name, $city, $country, $organization, $address, null, $postcode);
+    $contactinfo = new eppContact($postalinfo, $email, $telephone);
+    $contactinfo->setPassword('');
+    $contact = new verisignEppCreateContactRequest($contactinfo);
+    if ($response = $conn->request($contact)) {
+        /* @var $response Guanjia\EPP\eppCreateContactResponse */
+        echo "Contact created on " . $response->getContactCreateDate() . " with id " . $response->getContactId() . "\n";
+        return $response->getContactId();
+    } else {
+        echo "Create contact failed";
+    }
+    return null;
 }
